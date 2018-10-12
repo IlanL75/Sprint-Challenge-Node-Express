2ef4b109f4100a2ac9360fc29fb508958fae58e7
IlanL75 <anatoly.leytman@gmail.com>
2018-10-12 09:22:23 -0700

    Adding GET for actions list and projects list

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ server.get('/', (req, res) => {
     res.send('*** SERVER IS FUNCTIONAL ***');
 });
 
+server.get('/actions', (req, res) => {
+    actionModel.get()
+    .then((actions) => {
+       res.status(200).json(actions);
+    })
+    .catch((error) => {
+        res.status(500).json({message: 'The actions information could not be retrieved.'});
+    })
+});
+
+server.get('/projects', (req, res) => {
+    actionModel.get()
+    .then((projects) => {
+       res.status(200).json(projects);
+    })
+    .catch((error) => {
+        res.status(500).json({message: 'The projects information could not be retrieved.'});
+    })
+});
+
+
+
 
 const port = 8000;
 server.listen(port, () => {
     console.log(`\n=== API running on port ${port} ===\n`);
 });
+
+
